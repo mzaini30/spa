f59b36ae557547fbf310fe43b7d6008548bd24cb
mzaini30 <muhzaini30@gmail.com>
2021-06-14 16:48:58 +0800

    oke

--- app.go
+++ app.go
@@ -1,7 +1,9 @@
 package main
 import "net/http"
+import "fmt"
 import spa "github.com/roberthodgen/spa-server"
 
 func main() {
-    http.ListenAndServe(":8080", spa.SpaHandler(".", "index.html"))
+	fmt.Print("Silahkan buka localhost:2020")
+    http.ListenAndServe(":2020", spa.SpaHandler(".", "index.html"))
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module spa
+
+go 1.16
+
+require github.com/roberthodgen/spa-server v0.0.0-20171007154335-bb87b4ff3253 // indirect
