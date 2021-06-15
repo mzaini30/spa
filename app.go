5301816ea8a4cb169efb118d69f2d0327d434a08
mzaini30 <muhzaini30@gmail.com>
2021-06-15 11:20:38 +0800

    upload

--- app.go
+++ app.go
@@ -1,9 +1,31 @@
 package main
 import "net/http"
-import "fmt"
+// import "fmt"
 import spa "github.com/roberthodgen/spa-server"
+import "runtime"
+import "os/exec"
+// import "github.com/pkg/browser"
+
+func open(url string) error {
+    var cmd string
+    var args []string
+
+    switch runtime.GOOS {
+    case "windows":
+        cmd = "cmd"
+        args = []string{"/c", "start"}
+    case "darwin":
+        cmd = "open"
+    default: // "linux", "freebsd", "openbsd", "netbsd"
+        cmd = "xdg-open"
+    }
+    args = append(args, url)
+    return exec.Command(cmd, args...).Start()
+}
 
 func main() {
-	fmt.Print("Silahkan buka localhost:2020")
+	open("http://localhost:2020")
+	// fmt.Print("Silahkan buka localhost:2020")
+	// browser.OpenUrl('http://localhost:2020')
     http.ListenAndServe(":2020", spa.SpaHandler(".", "index.html"))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module spa
 
 go 1.16
 
-require github.com/roberthodgen/spa-server v0.0.0-20171007154335-bb87b4ff3253 // indirect
+require (
+	github.com/pkg/browser v0.0.0-20210606212950-a7b7a6107d32 // indirect
+	github.com/roberthodgen/spa-server v0.0.0-20171007154335-bb87b4ff3253 // indirect
+)
